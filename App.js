d488a54819cd7e3c76af904513009741ed48425e
Eva Despinoy <despinoy@kth.se>
2021-04-28 13:47:20 +0200

    added input field

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import Box from '@material-ui/core/Box';
 export default function App() {
 
   const [todosData, setTodosData] = useState([])
+  const [newTodo, setnewTodo] = useState("");
 
   // fetches all current to dos 
   function fetchTodosData() {
@@ -20,27 +21,42 @@ export default function App() {
     fetchTodosData();
   }, [])
 
+
+
   return (
     <Box position="absolute" top="15%" left="15%">
+      
+      <input placeholder="new to-do" onChange={() => setnewTodo(event.target.value)}></input>
+      <button onClick={() => addTodo(newTodo)} > Add To-Do </button>
+
       <Box p={1} margin="10px" textAlign="center" fontFamily="sans-serif">
         <h2>{"Current to-dos"}</h2>
       </Box>
       <div>{
         todosData.map(todo => 
           <Box bgcolor="#D6EBEF" p={1} margin="10px" textAlign="center" fontFamily="sans-serif">
-            <div>{todo.data}</div><button onClick={deleteTodo(todo.data)}> X </button>
+            <p>{todo.data + " "}<button onClick={deleteTodo(todo.data)}> X </button></p>
           </Box>
         )
       }</div>
+
     </Box>
   );
 }
 
-function deleteTodo(content) {
-  // TO DO 
-  console.log('hej')
-  console.log(content)
-}
+  function deleteTodo(content) {
+    console.log('delete')
+    console.log(content)
+    // todo: delete in local list
+    // todo: delete in db 
+  }
+  
+  function addTodo(content) {
+    console.log('add')
+    console.log(content)
+    // todo: delete in local list
+    // todo: delete in db 
+  }
 
 const styles = StyleSheet.create({
   container: {
